29839354983c66abba52f68eaa1986d18470bfe8
NinaMadu <136308860+NinaMadu@users.noreply.github.com>
2024-07-31 17:05:13 +0530

    again

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const user = require('./routers/user.route');
 const student = require('./routers/student.route');
+const auth = require('./milddleware/auth.js');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/user', user);
-app.use('/api/student', student);
+app.use('/api/student', auth, student);
 
 const port = process.env.PORT || 3000;
 
--- backend/routers/user.route.js
+++ backend/routers/user.route.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 
 const router = express.Router();
 
-router.post('login', (req, res) => {
-    res.send('User Logged in')
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const user = {name:username}
+    const token = jwt.sign(user,process.env.TOKEN_SECRET);
+    res.send({token});
 })
 
 module.exports = router
